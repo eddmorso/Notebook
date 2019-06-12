27e74e8a508fde5b3b678d282822f37f2e9cd272
Eduard <eddmorso@gmail.com>
2019-06-12 17:26:37 +0300

    add check methods for controller

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,20 +1,74 @@
 package Controller;
 
 import Model.Notebook;
+import Model.Subcriber.Address;
+import Model.Subcriber.Name;
 import View.Viewer;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Controller {
     private Notebook notebook;
     private Viewer viewer;
 
-    public Controller(Notebook notebook, Viewer viewer) {
+    public Controller(Viewer viewer, Notebook notebook) {
         this.notebook = notebook;
         this.viewer = viewer;
     }
 
     public void start(){
-
+        System.out.println("name " + checkName(new Name("Eduard", "t$", "Yuriyovich")));
+        System.out.println("email " + checkEmail("eddmorso@gmail.com"));
+        System.out.println("number " + checkPhoneNumber("+5482202"));
     }
 
+    private boolean checkName(Name name){
+        boolean firstName = Pattern.matches("[a-zA-Z]+", name.getName());
+        boolean secondName = Pattern.matches("[a-zA-Z]+", name.getSurname());
+        boolean lastName = Pattern.matches("[a-zA-Z]+", name.getLastName());
 
+        return firstName && secondName && lastName;
+    }
+
+    private boolean checkNickname(String nickname){
+        return Pattern.matches("[a-zA-Z0-9]+", nickname);
+    }
+
+//    public boolean checkGroup(){
+//        return false;
+//    }
+
+    private boolean checkComment(String comment){
+        return Pattern.matches("[a-zA-Z]*", comment);
+    }
+
+    private boolean checkTelephoneNumber(String number){
+        return Pattern.matches("[+]&&[0-9]*", number);
+    }
+
+    private boolean checkPhoneNumber(String number){
+        return Pattern.matches("[+]?[0-9]{7,}", number);
+    }
+
+    private boolean checkPhoneNumber2(String number){
+        return Pattern.matches("[+]?[0-9]+", number);
+    }
+
+    private boolean checkEmail(String email){
+        return Pattern.matches("[a-zA-Z&&[@]]+", email);
+    }
+
+    private boolean checkSkype(String skype){
+        return Pattern.matches("[a-zA-Z]*", skype);
+    }
+
+    private boolean checkAddress(Address address){
+        boolean city = Pattern.matches("[a-zA-Z]+", address.getCity());
+        boolean street = Pattern.matches("[a-zA-Z]+", address.getStreet());
+        boolean building = Pattern.matches("[a-zA-Z0-9]+", String.valueOf(address.getBuilding()));
+        boolean apartment = Pattern.matches("[0-9]+", String.valueOf(address.getApartment()));
+
+        return city && street && building && apartment;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,20 @@
+import Controller.Controller;
 import Model.Exceptions.DataException;
 import Model.Notebook;
 import Model.Subcriber.Address;
 import Model.Subcriber.Group;
 import Model.Subcriber.Name;
 import Model.Subcriber.Subscriber;
+import View.Viewer;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Subscriber subscriber = new Subscriber(new Name("Eduard", "Tsemenko",
-                "Yuriyovich"), "edmorso", Group.FAMILY,
-                1,1,"edts@fas", "sad",
-                new Address("Kyiv", "Balsaka", 2, 222), new Date());
         Notebook notebook = new Notebook();
 
-        try {
-            notebook.addSubscriber(subscriber);
-        }catch (DataException de){
-            de.printStackTrace();
-        }
+        Controller controller = new Controller(new Viewer(), notebook);
 
-        System.out.println(notebook.getSubscriber(subscriber));
-        System.out.println(subscriber.getName());
-        System.out.println(subscriber.getAddress());
-        System.out.println(subscriber.getCreationDate());
-
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        Subscriber subscriber1 = new Subscriber(new Name("Eduard", "Tsemenko",
-                "Yuriyovich"), "edmorso", Group.FAMILY,
-                1,1,"edts@fas", "sad",
-                new Address("Kyiv", "Balsaka", 2, 222), new Date());
-
-        try {
-            notebook.addSubscriber(subscriber1);
-        }catch (DataException de){
-            de.printStackTrace();
-        }
-
-        System.out.println(subscriber1.getCreationDate());
-        System.out.println(subscriber.getCreationDate());
+        controller.start();
     }
 }
